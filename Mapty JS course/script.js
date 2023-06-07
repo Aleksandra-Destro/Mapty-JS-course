4291812f495d1a433ba16ffd10929a858aa47230
Aleksandra-Destro <121759568+Aleksandra-Destro@users.noreply.github.com>
2023-06-07 08:59:15 +0200

    Update script.js

--- Mapty JS course/script.js
+++ Mapty JS course/script.js
@@ -12,7 +12,6 @@ class Workout {
   }
 
   _setDescription() {
-    // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
